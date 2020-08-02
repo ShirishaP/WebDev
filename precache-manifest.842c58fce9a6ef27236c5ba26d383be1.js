6b301dc559aee9310fa91ce5b8bc6dfed93bc625
ShirishaP <shirishapuchala@gmil.com>
2020-08-02 22:12:03 +0530

    Updates

---
+++ precache-manifest.842c58fce9a6ef27236c5ba26d383be1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dabab2e26d841d8acd28a8960ba3a2",
+    "url": "/WebDev/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/WebDev/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0f183256b93a3a10de5d",
+    "url": "/WebDev/static/css/main.dcbcabc3.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/WebDev/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WebDev/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f183256b93a3a10de5d",
+    "url": "/WebDev/static/js/main.65e0bedf.chunk.js"
+  },
+  {
+    "revision": "7c4e0077f08f2b715e06",
+    "url": "/WebDev/static/js/runtime-main.4b6ab056.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/WebDev/static/media/SEGA.12a3db8b.woff"
+  }
+]);
